f4e87c7dabe1422f91a8afaa1c2f364a68862891
Dustin Scarberry <bitsnbytes1001@gmail.com>
2026-03-04 18:36:22 -0500

    Fix invalid prop

--- src/component/Table/TableBody.jsx
+++ src/component/Table/TableBody.jsx
@@ -6,18 +6,18 @@ const TableBody = ({headers, data, searchable, search, sortable, sortColumn, sor
 
     // search filter
     if (searchable && search != '') {
-      filteredAndSortedData = filteredAndSortedData.filter(item => { 
+      filteredAndSortedData = filteredAndSortedData.filter(item => {
         const lowercaseSearch = search.toLowerCase();
 
         return item.rowData.some((s, index) => {
           // bypass search for non searchable columns
           if ('searchable' in headers[index] && headers[index].searchable == false)
             return false;
-      
+
           // use search value if specified
           if ('searchValue' in s)
             return s.searchValue.toLowerCase().includes(lowercaseSearch);
-          
+
           // search on base data
           return s.value.toLowerCase().includes(lowercaseSearch);
         });
@@ -40,7 +40,7 @@ const TableBody = ({headers, data, searchable, search, sortable, sortColumn, sor
         }
         else {
           if (sortDirection == 'asc') return 1;
-          if (sortDirection == 'desc') return -1; 
+          if (sortDirection == 'desc') return -1;
         }
       });
     }
@@ -53,12 +53,12 @@ const TableBody = ({headers, data, searchable, search, sortable, sortColumn, sor
   if (filteredAndSortedData.length == 0)
     return <tbody>
       <tr>
-        <td colspan="100%" style={{textAlign: 'center'}}>{noRecordsText}</td>
+        <td colSpan="100%" style={{textAlign: 'center'}}>{noRecordsText}</td>
       </tr>
     </tbody>
 
   return <tbody>
-    {filteredAndSortedData.map((row, i) => {    
+    {filteredAndSortedData.map((row, i) => {
       return <tr key={i}>
         {row.rowData.map((item, j) => {
           return <td key={j}>{item.value}</td>
